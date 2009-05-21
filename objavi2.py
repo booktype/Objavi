a5bbbc5c89b9eb804b87deabcd0c47ffaf45eb77
Douglas Bagnall <douglas@paradise.net.nz>
2009-05-21 14:03:11 +1200

    generate table of contents (not working)

--- objavi2.py
+++ objavi2.py
@@ -3,12 +3,20 @@
 import os, sys
 import cgi
 import re
-import urllib
+from urllib2 import urlopen
 from getopt import gnu_getopt
+
+import lxml
+
 
 ENV = os.environ()
 SERVER_NAME = ENV.get('SERVER_NAME', 'en.flossmanuals.net')
 
+TOC_URL = "http://%s/pub/%%s/_index/TOC.txt" % SERVER_NAME
+BOOK_URL = "http://%s/bin/view/%%s/_all?skin=text" % SERVER_NAME
+
+# ARG_VALIDATORS is a mapping between the expected cgi arguments and
+# functions to validate their values. (None means no validation).
 ARG_VALIDATORS = {
     "webName": lambda x: '/' not in x and '..' not in x,
     "css": None,
@@ -18,7 +26,7 @@ ARG_VALIDATORS = {
     "license": None,
 }
 
-__doc__ += 'Valid arguments are: ' + ', '.join(ARG_VALIDATORS.keys())
+__doc__ += '\nValid arguments are: %s.\n' % ', '.join(ARG_VALIDATORS.keys())
 
 def log(*messages):
     for m in messages:
@@ -28,11 +36,11 @@ def parse_args():
     """Read and validate CGI or commandline arguments, putting the
     good ones into the returned dictionary.  Command line arguments
     should be in the form --title='A Book'.
-    """    
-    query = cgi.FieldStorage
+    """
+    query = cgi.FieldStorage()
     options, args = gnu_getopt(sys.argv[1:], '', [x + '=' for x in ARG_VALIDATORS])
     options = dict(options)
-    
+
     data = {}
     for key, validator in ARG_VALIDATORS.items():
         value = query.getfirst(key, options.get(key, None))
@@ -45,10 +53,22 @@ def parse_args():
     return data
 
 
-TOC_PATH="/var/www/write/pub/%s/_index/TOC.txt"
-
 def toc_reader(name):
-    f = open(TOC_PATH % name)
+    """Iterate over the TOC.txt file.  For each chapter yield the
+    tuple (<status>, <filename>, <title>).
+
+    <status> is a number, with the following meaning:
+
+          0 - section heading with no chapter
+          1 - chapter heading
+          2 - book title
+
+    <filename> is twiki name of the chapter.
+
+    <title> is a human readable title for the chapter.  It is likely to
+    differ from the title given in the chapter's <h1> heading.
+    """
+    f = urlopen(TOC_URL % name)
     while True:
         try:
             yield (f.next(), f.next(), f.next())
@@ -56,80 +76,101 @@ def toc_reader(name):
             break
     f.close()
 
-def get_book(name):
-    stream = urlopen("http://%s/bin/view/%s/_all?skin=basic" % (SERVER_NAME, name))
-    html = stream.read()
+
+ADD_TEMPLATE = False
+
+def get_book(name, tidy=True):
+    """Fetch and parse the raw html of the book.  If tidy is true
+    (default) links in the document will be made absolute."""
+    #XXX use lxml
+    f = urlopen(BOOK_URL % name)
+    html = f.read()
+    if ADD_TEMPLATE:
+        html = ('<html><head>\n<title>book</title>\n'
+                '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >'
+                '</head>\n<body>\n'
+                '%s\n</body></html>') % html
+    f.close()
+    tree = lxml.html.document_fromstring(html)
+    tree.make_links_absolute(BOOK_URL % name)
+
+    return tree
 
 
-
-def make_pdf(html, name):
+def make_pdf(htmltree):
     data = []
     current_heading = 1
     num_heading = 0
     chapters = []
 
-def make_toc(html, name):
-    toc = []
+def find_page(element, start=1):
+    """Search through the main PDF and retunr the page on which the
+    element occurs.  If start is given, the search begins on that
+    page."""
+    #XXX do it really.
+    import random
+    return start + random.randrange(1,4)
 
-    for status, chapter_url, desc in toc_reader(name):
+def make_contents(htmltree, toc):
+    header_tmpl = '<h1>%s</h1><table class="toc">\n'
+    row_tmpl = ('<tr><td class="chapter">%s</td><td class="title">%s</td>'
+                '<td class="page_num">%s</td></tr>\n')
+    section_tmpl = ('<tr><td></td><td class="section" colspan="2">%s</td></tr>\n')
+    footer = '\n</table><small>TOC ends here</small>'
+
+    contents = []
+    headers = []
+
+    chapter = 1
+    page_num = 1
+    subsections = [] # for the subsection heading pages.
+
+    headings =  htmltree.cssselect('h1')
+
+    for status, chapter_url, text in toc:
         # status is
         #  0 - section heading with no chapter
         #  1 - chapter heading
         #  2 - book title
         #
         # chapter_url is twiki name of the chapter
-        # desc is the human readable name of the chapter.
-        # XXX it may differ from the H1
-        if status == '0':
-            if current_heading + num_heading != 1:
-	        data.append(_showHeading(web, current_heading, chapters))
-	        chapters = []
-            current_heading += num_heading
-            num_heading = 0
-            data.append('<h0>%s</h0>\n<div class="fmtoc">' % desc)
+        # text is a human readable name of the chapter.
+        if status == '2':
+            headers.append(header_tmpl % text)
 
         elif status == '1':
-            chapters.append(chapter_url)
-            data.append('%s%s<br />\n' % (current_heading + num_heading, description))
-            num_heading += 1
+            h1 = headings.next()
+            title = h1.text_content()
+            page_num = find_page(h1, page_num)
+            contents.append(row_tmpl % (chapter, title, page_num))
+            #put a bold number at the beginning of the h1
+            initial = h1.makeelement("strong", Class="initial")
+            h1.insert(0, initial)
+            initial.tail = h1.text
+            initial.text = "%s." % chapter
+            h1.text = ''
 
+            chapter += 1
 
-    data.append(_showHeading(web, current_heading, chapters))
+        elif status == '0':
+            contents.append(section_tmpl % text)
+            #XXX interstitial pages need to be made with the main PDF
 
+        else:
+            log("mystery TOC line: %s %s %s" % (status, chapter_url, text))
 
-
-#XXX need to do this differently
-# 1. get whole html
-# 2. fix headings, remove META (in python or javascript)
-
-
-def _showHeading(web, num_heading, chapters):
-    html = []
-
-    html.append("</div></fmsection><p></p>\n")
-
-    n = 0
-    for chapter in chapters:
-        topicData = readTopicText(web, chapter)
-        topicData = expandCommonVariables(topicData, "neki topic", web)
-        topicData = renderText(topicData, web)
-        for line in topicData.split('/n'):
-            if '<h1>' in line:
-                html.append(line.replace('<h1>', '<h1><span class="fminitial">%s</span>' % num_heading + n))
-                n += 1
-            else :
-                html.append(re.sub('/%META:\w+{.*?}%/', '/', line))
-
-   return ''.join(html)
-
-
-
+    doc = '\n'.join(headers) + '\n'.join(contents) + footer
+    #return doc
 
 
 
 if __name__ == '__main__':
     args = parse_args()
     web_name = args['webHome']
-    html = get_book(web_name)
+    htmltree = get_book(web_name)
+    pdfname = make_pdf(htmltree)
+    
+    toc = toc_reader(web_name)
+    contents = make_contents(htmltree, toc)
 
-    pdfname = make_pdf(html, manual)
+
