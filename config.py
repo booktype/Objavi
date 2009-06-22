ac8e3df2f7dbcb269c5df15201828793ec123a48
Douglas Bagnall <douglas@paradise.net.nz>
2009-06-22 21:46:30 +1200

    CSS tuning, and put default CSS in form

--- config.py
+++ config.py
@@ -24,6 +24,10 @@ CHAPTER_COOKIE_CHARS = 'BCDFGHJKLMNPQRSTVWXYZ'
 #by a cgi script
 
 DEFAULT_SERVER = 'en.flossmanuals.net'
+DEFAULT_SIZE = 'COMICBOOK'
+DEFAULT_ENGINE = 'webkit'
+#DEFAULT_MODE = None
+
 #DEFAULT_CSS = 'file://' + os.path.abspath('static/default.css')
 SERVER_DEFAULTS = {
     'en.flossmanuals.net': {
