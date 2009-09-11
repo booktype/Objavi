779ae513f311370dbdae802d13cce158cc86697f
Douglas Bagnall <douglas@paradise.net.nz>
2009-09-11 21:47:09 +1200

    hacking away on OPF parser.

--- epub.py
+++ epub.py
@@ -1,21 +1,24 @@
 """Module for dealing with epub -> booki conversions."""
 
 import os, sys
-#from urllib2 import urlopen
+from pprint import pprint
 import zipfile
 from cStringIO import StringIO
 
 import lxml, lxml.html, lxml.etree, lxml.cssselect
 
+NAMESPACES = {
+    'opf': 'http://www.idpf.org/2007/opf',
+    'dc': 'http://purl.org/dc/elements/1.1/', #dublin core
+    'tei': 'http://www.tei-c.org/ns/1.0',
+    'dcterms': 'http://purl.org/dc/terms/',
+    'nzetc': 'http://www.nzetc.org/structure',
+    'xsi': 'http://www.w3.org/2001/XMLSchema-instance'
+}
+
 class EpubError(Exception):
     pass
 
-def string_tree(s):
-    f = StringIO(s)    
-    tree = lxml.etree.parse(f)
-    f.close()
-    return tree
-    
 
 class Epub(object):
     """
@@ -64,7 +67,7 @@ class Epub(object):
 
         Other files are allowed in META-INF, but none of them are much
         use.  They are manifest.xml, metadata.xml, signatures.xml,
-        encryption.xml, and rights.xml.        
+        encryption.xml, and rights.xml.
         '''
         xml = self.zip.read('META-INF/container.xml')
         tree = lxml.etree.XML(xml)
@@ -73,9 +76,122 @@ class Epub(object):
                 rootfile = r.attrib['full-path']
                 break
         else:
-            raise EpubError("No OEPBS rootfile found")
+            raise EpubError("No OPF rootfile found")
 
-        self.oepbs_root = rootfile
+        self.opf_file = rootfile
+
+    def zip2tree(self, name):
+        """get an etree from the given zip filename"""
+        #Note: python 2.6 (not 2.5) has zipfile.open
+        s = self.zip.read(name)
+        f = StringIO(s)
+        tree = lxml.etree.parse(f)
+        f.close()
+        return tree
+
+
+    def parse_opf(self):
+        """
+        <metadata>
+        <dc:title>A Treatise of Human Nature</dc:title>
+        <dc:creator>David Hume</dc:creator>
+        <dc:date>2008-09-04</dc:date>
+        <dc:subject>Philosophy</dc:subject>
+        <dc:language>en</dc:language>
+        <dc:publisher>web-books.com</dc:publisher>
+        <dc:identifier id="BookId">web-books-1053</dc:identifier>
+        </metadata>
+        <manifest>
+        <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml" />
+        <item id="W000Title" href="000Title.html" media-type="application/xhtml+xml" />
+        <item id="WHume_NatureC01" href="Hume_NatureC01.html" media-type="application/xhtml+xml" />
+        <item id="style" href="style.css" media-type="text/css" />
+        <item id="cover" href="cover.jpg" media-type="image/jpeg" />
+        </manifest>
+        <spine toc="ncx">
+        <itemref idref="W000Title" />
+        <itemref idref="WHume_NatureC01" />
+        </spine>
+        """
+        tree = self.zip2tree(self.opf_file)
+        root = tree.getroot()
+        #print tree.getroot().nsmap
+        nsmap = {'opf': 'http://www.idpf.org/2007/opf'}
+        metadata = root.xpath('.//opf:metadata', namespaces=nsmap)[0]
+        manifest = root.xpath('.//opf:manifest', namespaces=nsmap)[0]
+        spine = root.xpath('.//opf:spine', namespaces=nsmap)[0]
+
+        md = parse_metadata(metadata)
+
+
+        return md
+
+
+
+def parse_metadata(metadata, nsmap=None, recurse=True):
+    """metadata is an OPF metadata node, as defined at
+    http://www.idpf.org/2007/opf/OPF_2.0_final_spec.html#Section2.2
+    (or a dc-metadata or x-metadata child thereof).
+
+    """
+    if nsmap is None:
+        nsmap = metadata.nsmap
+
+    #print nsmap
+    # the node probably has at least dc, opf, and None namespace prefixes.
+    # None and opf probably map to the same thing.
+    # We ultimately don't care about the prefixes because they are local to the file.
+    nsdict = dict((v, {}) for v in nsmap.values())
+    pfdict = dict((k, nsdict[v]) for k, v in nsmap.iteritems())
+    nstags = dict((k, '{%s}' % v) for k, v in nsmap.iteritems())
+    default_ns = nstags[None]
+
+    for t in metadata.iterchildren():
+        #look for special OPF tags
+        if t.tag == nstags[None] + 'meta':
+            #meta tags <meta name="" content="" />
+            #del t.nsmap[None]
+            #print lxml.etree.tostring(t)
+            name = t.get('name')
+            content = t.get('content')
+            others = [(k, v) for k, v in t.items() if k not in ('name', 'content')]
+            prefix = None
+            if ':' in name:
+                # the meta tag is using xml namespaces
+                prefix, name = name.split(':', 1)
+            pfdict.get(prefix, pfdict[None])[name] = (content, others)
+            continue
+
+        if t.tag == nstags[None] + 'dc-metadata' and recurse:
+            #contents are DC metadata tags, which means (i think) we
+            #need to look for sub-elements and set their namespace to
+            #DC.  There should only be one level of nesting, so set
+            #recurse to False.
+            submap = nsmap.copy()
+            submap[None] = NAMESPACES['dc']
+            subdict = parse_metadata(t, submap, recurse=False)
+            for k, v in subdict.items():
+                if k not in nsdict:
+                    nsdict[k] = {}
+                nsdict[k].update(v)
+            continue
+
+        if t.tag == nstags[None] + 'x-metadata' and recurse:
+            #contents are non-dc-metadata tags.  Namespace is presumably unchanged.
+            subdict = parse_metadata(t, nsmap, recurse=False)
+            for k, v in subdict.items():
+                if k not in nsdict:
+                    nsdict[k] = {}
+                nsdict[k].update(v)
+            continue
+        tag = t.tag.replace('{' + nsmap[t.prefix] + '}', '')
+
+        #any key can be duplicate, so store in a list
+        values = pfdict[t.prefix].setdefault(tag, [])
+        values.append((t.text, tuple((k.replace(default_ns, ''), v) for k, v in t.items())))
+
+    return nsdict
+
 
 
 
--- tests/_epub.py
+++ tests/_epub.py
@@ -4,11 +4,36 @@
 
 import os, sys
 import tempfile
-
+from pprint import pprint
 import epub
 
 TEST_FILE_DIR = 'tests/epub-examples/'
 TEST_FILES =  sorted( TEST_FILE_DIR + x for x in os.listdir(TEST_FILE_DIR) )
+#print '\n'.join(TEST_FILES)
+
+# Best_of_TOC.epub
+# Bonaparte.epub
+# Cowan-Kimberly.epub
+# GalCuri.epub
+# Hume_Nature.epub
+# LittleBrother.epub
+# Treasure_Island.epub
+# beaglehole-letter-61.epub
+# cowan-to-fildes.epub
+# ctaquarterly13197477chic.epub
+# cyclopedia-wellington.epub
+# darwin-autobiography-of-charles-darwin.epub
+# early-life-notes.epub
+# halfhoursinfarno00newy.epub
+# littleroadstoryo00hick.epub
+# official-history-nz.epub
+# songssourdough00servuoft.epub
+# stevenson-black-arrow.epub
+# stevenson-calibre-pathological.epub
+# takitimu.epub
+# war-economy-recipes.epub
+# wells-calibre-pathological.epub
+# wells-war-of-the-worlds.epub
 
 def _test_file(x):
     if isinstance(x, int):
@@ -21,18 +46,19 @@ def _test_file(x):
 
 #TEMPDIR = tempfile.mkdtemp(prefix='epub-')
 
-def _load_epub(filename='Hume'):
+def _load_epub(filename, verbose=False):
     fn = _test_file(filename)
-    print fn
+    if fn is None:
+        raise ValueError("'%s' doesn't refer to a known file")
+    if verbose:
+        print fn
     e = epub.Epub()
     e.load(open(fn).read())
     return e
 
-
-
 def test_load():
     fn = _test_file('Treasure_Island')
-    print fn
+    #print fn
     e = epub.Epub()
     e.load(open(fn).read())
     assert e.names
@@ -42,18 +68,58 @@ def test_load():
 def test_meta():
     for book, root in [('Hume', 'OPS/content.opf'),
                        ('Treasure', 'OEBPS/volume.opf'),
-                       ('arrow', "OPS/epb.opf")
+                       ('arrow', "OPS/epb.opf"),
+                       ('LittleBrother', "metadata.opf"),
                        ]:
         e = _load_epub(book)
         e.parse_meta()
-        assert e.oepbs_root == root
-    
+        assert e.opf_file == root
+
+
+def test_opf():
+    for book in ['ctaquarterly', 'letter-61',
+                 'early-life', 'LittleBrother'
+                 ]:
+        e = _load_epub(book, verbose=True)
+        e.parse_meta()
+        metadata = e.parse_opf()
+        pprint(metadata)
 
 
 
+def test_parse_metadata():
+    print "TESTING metadata"
+    import lxml
+    f = open('tests/metadata.xml')
+    tree = lxml.etree.parse(f)
+    f.close()
+    #nsmap = {'opf': 'http://www.idpf.org/2007/opf'}
+    #metadata = root.xpath('.//opf:metadata', namespaces=nsmap)[0]
+
+    for metadata in tree.iter('{http://www.idpf.org/2007/opf}metadata'):
+        pprint(epub.parse_metadata(metadata))
 
 
 
 #test_load()
-    
 
+
+
+
+
+
+
+
+DC_METATAGS = ['abstract', 'accessRights', 'accrualMethod', 'accrualPeriodicity',
+               'accrualPolicy', 'alternative', 'audience', 'available', 'bibliographicCitation',
+               'conformsTo', 'contributor', 'coverage', 'created', 'creator', 'date',
+               'dateAccepted', 'dateCopyrighted', 'dateSubmitted', 'description',
+               'educationLevel', 'extent', 'format', 'hasFormat', 'hasPart',
+               'hasVersion', 'identifier', 'instructionalMethod', 'isFormatOf',
+               'isPartOf', 'isReferencedBy', 'isReplacedBy', 'isRequiredBy',
+               'issued', 'isVersionOf', 'language', 'license', 'mediator',
+               'medium', 'modified', 'provenance', 'publisher', 'references',
+               'relation', 'replaces', 'requires', 'rights', 'rightsHolder',
+               'source', 'spatial', 'subject', 'tableOfContents', 'temporal',
+               'title', 'type', 'valid'
+               ]
