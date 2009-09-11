16c9f86878beadc159acb52381b891bec9962f9d
Douglas Bagnall <douglas@paradise.net.nz>
2009-09-11 21:46:06 +1200

    script to unzip all the epubs for inspection, but don't add them to git

---
+++ tests/unzip.sh
@@ -0,0 +1,18 @@
+#/bin/bash -e
+
+# Unpack all the test epub files into
+# tests/epub-examples/unzipped/<epub-name>
+
+TESTDIR=`dirname $0`
+
+cd $TESTDIR/epub-examples
+
+[[ -d unzipped ]] || mkdir unzipped
+
+for f in *.epub; do
+    #echo $f
+    d=unzipped/${f/.epub/}
+    [[ -d $d ]] && continue
+    mkdir $d
+    unzip $f -d $d
+done
