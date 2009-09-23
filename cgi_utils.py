217a935458614dd09efb8380f5dcb8e97abc1516
Douglas Bagnall <douglas@paradise.net.nz>
2009-09-23 23:12:47 +1200

    begin moving some of the common CGI code into a library

---
+++ cgi_utils.py
@@ -0,0 +1,47 @@
+# Part of Objavi2, which turns html manuals into books
+#
+# Copyright (C) 2009 Douglas Bagnall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import os, sys
+import cgi
+from getopt import gnu_getopt
+
+from fmbook import log
+import config
+
+
+def parse_args(arg_validators):
+    """Read and validate CGI or commandline arguments, putting the
+    good ones into the returned dictionary.  Command line arguments
+    should be in the form --title='A Book'.
+    """
+    query = cgi.FieldStorage()
+    options, args = gnu_getopt(sys.argv[1:], '', [x + '=' for x in arg_validators])
+    options = dict(options)
+    data = {}
+    for key, validator in arg_validators.items():
+        value = query.getfirst(key, options.get('--' + key, None))
+        log('%s: %s' % (key, value), debug='STARTUP')
+        if value is not None:
+            if validator is not None and not validator(value):
+                log("argument '%s' is not valid ('%s')" % (key, value))
+                continue
+            data[key] = value
+
+    log(data, debug='STARTUP')
+    return data
+
