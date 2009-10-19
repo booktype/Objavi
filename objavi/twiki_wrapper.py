c38afbf2f2ce6bbe246734b7febffcf2a919bb68
Douglas Bagnall <douglas@paradise.net.nz>
2009-10-20 07:18:43 +1300

    revert the encoding patch with comments so that it can be fixed

--- objavi/twiki_wrapper.py
+++ objavi/twiki_wrapper.py
@@ -58,12 +58,15 @@ def toc_iterator(server, book):
     url = config.TOC_URL % (server, book)
     log('getting TOC: %s' % url)
     f = urlopen(url)
-    encoding = config.SERVER_DEFAULTS[server]['toc-encoding']
+    #encoding = config.SERVER_DEFAULTS[server]['toc-encoding']
     while True:
         try:
-            yield (f.next().decode(encoding).strip(),
-                   f.next().decode(encoding).strip(),
-                   f.next().decode(encoding).strip())
+            yield (f.next().strip(),
+                   f.next().strip(),
+                   f.next().strip())
+            #yield (f.next().decode(encoding).strip(),
+            #       f.next().decode(encoding).strip(),
+            #       f.next().decode(encoding).strip())
         except StopIteration:
             break
     f.close()
