0ba1705ea215fdfff9bd8a813c7b63ab3f70bd10
Douglas Bagnall <douglas@paradise.net.nz>
2010-02-27 07:35:32 +1300

    moving web stuff into htdocs directory, to allow for secrets

--- pdf/demargin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# remove margins form PDF files.
-
-pdfcrop gnu-wk-lucida.pdf gnu-wk-lucida-CROP.pdf
-pdfcrop --margins "20 20 20 20" gnu-wk-lucida.pdf gnu-wk-lucida-MARGIN-20.pdf
-
-
--- pdf/pdfedit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-##
-
-pdfedit -d 5 -s left-right-margins-pdfedit.js go_go_go $1
-
