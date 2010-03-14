5231e8667c43c8de15daee7107cf03524455d384
Douglas Bagnall <douglas@paradise.net.nz>
2010-03-15 00:52:45 +1300

    javascript and css fixes for html templating engine.

--- htdocs/static/templating_template_flossmanuals.js
+++ htdocs/static/templating_template_flossmanuals.js
@@ -1,106 +1,47 @@
-function addLoadEvent(func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function() {
-            oldonload();
-            func();
-        };
-    }
-}
 
-function insertAfter(newElement, targetElement) {
-    var parent = targetElement.parentNode;
-    if (parent.lastChild == targetElement) {
-        parent.appendChild(newElement);
-    } else {
-        parent.insertBefore(newElement, targetElement.nextSibling);
-    }
-}
-
-function captionizeImages() {
-    if (!document.getElementsByTagName ||
-        !document.createElement) {
-        return false;
-    }
-    var images = document.getElementsByTagName("img");
-    for (var i=0; i<images.length; i++) {
-        if (images[i].className != "non") {
-            var title = images[i].getAttribute("title");
-            var width = images[i].width;
-            var divCaption = document.createElement("div");
-            divCaption.className="caption";
-            divCaption.style.width=width+'px';
-            if (title){
-                divCaption.style.padding='2px 0px 3px 0px';
+function get_chapter_list() {
+    var chapters = [];
+    var here = document.location;
+    $(".menu-goes-here a").each(function (i) {
+                                    if (this.href == here){
+                                        $(this).parent().css('background', '#f99b1c');
+                                    }
+                                    chapters.push(this.href);
+                                });
+    var  prev, next;   
+    for (var i = 0; i < chapters.length; i++){
+        if (chapters[i] == here){
+            if (i > 0){
+                $('.left.arrow a').each(function() {
+                                            this.href = chapters[i-1];
+                                        });
             }
-            divCaption.style.display='block';
-            var divCaption_text = document.createTextNode(title);
-            divCaption.appendChild(divCaption_text);
-            var divContainer = document.createElement("div");
-            divContainer.className="imgcontainer";
-            if (title){
-                divContainer.style.padding='0px 0px 10px 0px';
+            else{
+                $('.left.arrow a').each(function() {
+                                            this.style.visibility = 'hidden';
+                                        });                
             }
-            images[i].parentNode.insertBefore(divContainer,images[i]);
-            divContainer.appendChild(images[i]);
-            insertAfter(divCaption,images[i]);
+            if (i + 1 < chapters.length){
+                $('.right.arrow a').each(function() {
+                                             this.href = chapters[i+1];
+                                         });
+                }
+            else{
+                $('.right.arrow a').each(function() {
+                                             this.style.visibility = 'hidden';
+                                         });              
+            }
         }
     }
-}
-//addLoadEvent(captionizeImages);
+    return chapters;
+}    
 
-function next () {
-    var onode, otarget;
-    onode=document.getElementById("%TOPIC%");
-    if (onode.id=="Credits") die;
-    //alert (onode.id);
-    onode=onode.nextSibling;
-    if (onode.id=="heading") onode=onode.nextSibling;
-    while (onode) {
-        //onode=onode.nextSibling;
-        if (onode.nodeType==1) {
-	    //alert (onode.id);
-            otarget=onode;
-            break;
-        }
-        onode=onode.nextSibling;
-    }
-    if (otarget) {
-        //you actually have found one, and do something here
-        //alert(otarget.id + "\n" + otarget.tagName);    //just to verify
-        top.location = "/" + otarget.id;
-    } else {
-        //you don't find one
-        //alert("nada" + otarget.id);
-    }
+var all_book_chapters = $(get_chapter_list);
+
+function previous(){
+    return true;
 }
 
-function previous () {
-    var onode, otarget;
-    onode=document.getElementById("/");
-    if (onode.id=="Introduction") die;
-    //alert (onode.id);
-    onode=onode.previousSibling;
-    if (onode.id=="heading") onode=onode.previousSibling;
-    if (onode.id=="title") die;
-    while (onode) {
-        //onode=onode.previousSibling;
-        if (onode.nodeType==1) {
-	    //alert (onode.id);
-            otarget=onode;
-            break;
-        }
-        onode=onode.previousSibling;
-    }
-    if (otarget) {
-        //you actually have found one, and do something here
-        //alert(otarget.id + "\n" + otarget.tagName);    //just to verify
-        top.location =  otarget.id;
-    } else {
-        //you don't find one
-        //alert("nada" + otarget.id);
-    }
+function next(){
+    return true;
 }
-
