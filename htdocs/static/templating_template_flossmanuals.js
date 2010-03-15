2dc69482f1afa11681db5b4c5a725652f4304795
Douglas Bagnall <douglas@paradise.net.nz>
2010-03-15 15:22:20 +1300

    further simplification of html templates, with better RTL support

--- htdocs/static/templating_template_flossmanuals.js
+++ htdocs/static/templating_template_flossmanuals.js
@@ -1,47 +1,48 @@
 
-function get_chapter_list() {
+function set_up_chapter_links() {
     var chapters = [];
+    var titles = [];
     var here = document.location;
+    if (/\/$/.test(here)){
+        here += 'index.html';
+    }
+
     $(".menu-goes-here a").each(function (i) {
                                     if (this.href == here){
                                         $(this).parent().css('background', '#f99b1c');
                                     }
                                     chapters.push(this.href);
+                                    titles.push($(this).text());
                                 });
-    var  prev, next;   
+    var hide = function(){
+        this.style.visibility = 'hidden';
+    };
+
     for (var i = 0; i < chapters.length; i++){
         if (chapters[i] == here){
             if (i > 0){
                 $('.left.arrow a').each(function() {
-                                            this.href = chapters[i-1];
+                                            this.href = chapters[i - 1];
+                                            this.title = titles[i - 1];
                                         });
             }
             else{
-                $('.left.arrow a').each(function() {
-                                            this.style.visibility = 'hidden';
-                                        });                
+                $('.left.arrow a').each(hide);
             }
             if (i + 1 < chapters.length){
                 $('.right.arrow a').each(function() {
-                                             this.href = chapters[i+1];
+                                             this.href = chapters[i + 1];
+                                             this.title = titles[i + 1];
                                          });
                 }
             else{
-                $('.right.arrow a').each(function() {
-                                             this.style.visibility = 'hidden';
-                                         });              
+                $('.right.arrow a').each(hide);
             }
         }
     }
+    //alert(titles);
     return chapters;
-}    
-
-var all_book_chapters = $(get_chapter_list);
-
-function previous(){
-    return true;
 }
 
-function next(){
-    return true;
-}
+var all_book_chapters = $(set_up_chapter_links);
+
