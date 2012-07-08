64c12d132af467dfaf187d8a113463f4822d9253
Borko Jandras <borko.jandras@sourcefabric.org>
2012-07-08 17:08:19 +0200

    Initial PDF generation through Django interface.

--- lib/objavi/views.py
+++ lib/objavi/views.py
@@ -22,10 +22,145 @@ from django.template import RequestContext
 import forms
 import form_config
 import config
+import fmbook
 import book_utils
 import booki_wrapper
 import twiki_wrapper
 import settings
+
+from objavi.pdf import resize_pdf, count_pdf_pages
+
+
+class ObjaviRequest(object):
+    def __init__(self, args):
+        self.bookid = args.get('book')
+        self.server = args.get('server')
+        self.mode = args.get('mode', 'book') #XXX default should be configured?
+        extension = form_config.CGI_MODES.get(self.mode)[1]
+        self.bookname = book_utils.make_book_name(self.bookid, self.server, extension)
+        self.destination = args.get('destination')
+        self.callback = args.get('callback')
+        self.method = args.get('method', form_config.CGI_DESTINATIONS[self.destination]['default'])
+        self.template, self.mimetype = form_config.CGI_DESTINATIONS[self.destination][self.method]
+        self.bookurl = "%s/books/%s" % (config.OBJAVI_URL, self.bookname,)
+
+        if args.get('output_format') and args.get('output_profile'):
+            self.bookurl = self.bookurl.rsplit(".", 1)[0]+"."+args.get('output_format')
+
+        self.details_url, self.s3url = fmbook.find_archive_urls(self.bookid, self.bookname)
+        self.booki_group = args.get('booki_group')
+        self.booki_user = args.get('booki_user')
+
+    def finish(self, book):
+        book.publish_shared(self.booki_group, self.booki_user)
+        if self.destination == 'archive.org':
+            book.publish_s3()
+
+    def log_notifier(self, message):
+        print('*** MESSAGE: "%s"' % message)
+
+    def get_watchers(self):
+        return set([self.log_notifier])
+
+
+def parse_request(request):
+    """Extracts arguments from the HTTP request.
+    """
+    # copy argument values from the request, using defaults when missing
+    #
+    args = {}
+    for input, _, _, _, _, _, _, default in form_config.FORM_INPUTS:
+        args[input] = request.REQUEST.get(input, default)
+
+    # rename some arguments to a canonical form
+    #
+    MAPPINGS = {
+        "booki-user"   : "booki_user",
+        "booki-group"  : "booki_group",
+        "page-numbers" : "page_numbers",
+        "max-age"      : "max_age",
+        "embed-fonts"  : "embed_fonts",
+        "allow-breaks" : "allow_breaks",
+        }
+    for old_key, new_key in MAPPINGS.items():
+        if args.has_key(old_key):
+            args[new_key] = args[old_key]
+            del args[old_key]
+
+    # validate input
+    #
+    form = forms.ObjaviForm(args)
+    if form.is_valid():
+        args = form.cleaned_data
+    else:
+        return None
+
+    destination = request.REQUEST.get("destination", form_config.DEFAULT_CGI_DESTINATION)
+    if destination in form_config.CGI_DESTINATIONS.keys():
+        args["destination"] = destination
+
+    engine = request.REQUEST.get("engine", config.DEFAULT_ENGINE)
+    if engine in config.ENGINES.keys():
+        args["engine"] = engine
+
+    return args
+
+
+def make_book(context, args):
+    """Creates a Book instance for the specified request context and request arguments.
+    """
+    book_args = {
+        "page_settings"     : book_utils.get_page_settings(args),
+        "watchers"          : context.get_watchers(),
+        "license"           : args.get("license"),
+        "max_age"           : float(args.get("max_age")),
+        "page_number_style" : args.get("page-numbers"),
+        }
+
+    if args.get("isbn"):
+        book_args["isbn"] = args.get("isbn")
+    if args.get("title"):
+        book_args["title"] = args.get("title")
+
+    book = fmbook.Book(context.bookid, context.server, context.bookname, **book_args)
+
+    toc_header = args.get("toc_header")
+    if toc_header:
+        book.toc_header = toc_header
+
+    return book
+
+
+def mode_book(request):
+    args = parse_request(request)
+    context = ObjaviRequest(args)
+
+    with make_book(context, args) as book:
+        book.spawn_x()
+        book.load_book()
+
+        if not args.get('allow-breaks'):
+            book.fake_no_break_after()
+
+        book.add_css(args.get('css'), context.mode)
+        book.add_section_titles()
+
+        if context.mode == 'book':
+            book.make_book_pdf()
+        elif context.mode in ('web', 'newspaper'):
+            book.make_simple_pdf(context.mode)
+
+        if args.get("rotate"):
+            book.rotate180()
+
+        if args.get('embed-fonts'):
+            log("embedding fonts!")
+            book.embed_fonts()
+
+        book.publish_pdf()
+        context.finish(book)
+
+    return HttpResponse(context.bookurl)
 
 
 def mode_booklist(request):
@@ -58,10 +193,6 @@ def mode_form(request):
     return render_to_response("form.html", context, context_instance=RequestContext(request))
 
 
-def mode_pdf(request):
-    return HttpResponse("")
-
-
 def default(request):
     mode = request.REQUEST.get("mode")
 
@@ -75,7 +206,7 @@ def default(request):
     if book and not mode:
         mode = "book"
 
-    if mode == "book":
-        return mode_pdf(request)
+    if mode in ("book", "newspaper", "web"):
+        return mode_book(request)
     else:
         return mode_form(request)
