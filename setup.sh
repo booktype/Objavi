514fea8c8f789cdc4f2a152cb374a6b7baa70d97
Borko Jandras <borko.jandras@sourcefabric.org>
2012-08-30 14:39:49 +0200

    Updated the setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# cobbled together from history
 
-mkdir tmp
-chmod 777 tmp
-mkdir books
-mkdir cache
-sudo chgrp www-data books
-sudo chgrp www-data cache
-sudo chmod 775 books cache
-sudo chmod g+s books cache
+mkdir -m 775 htdocs/books
+mkdir -m 775 htdocs/booki-books
+mkdir -m 775 htdocs/tmp
+mkdir -m 775 cache
+mkdir -m 775 log
+
+sudo chgrp -R www-data htdocs/books
+sudo chgrp -R www-data htdocs/booki-books
+sudo chgrp -R www-data htdocs/tmp
+sudo chgrp -R www-data cache
+sudo chgrp -R www-data log
+
+chmod 775 htdocs/*.cgi htdocs/font-list.cgi.pdf
+sudo chgrp www-data htdocs/*.cgi htdocs/font-list.cgi.pdf
